5cc43ec6b24e4c3118606e09f997474e0accf28b
63070117 <74042093+63070117@users.noreply.github.com>
2020-11-06 15:49:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <html>
     <head>
-        <title>Hello world</title>
+        <title>Pitpiboon Ruenyen 63070117</title>
     </head>
     <body>
-        <h1>My First Heading</h1>
-        <P>My First Paragraph</P>
-        <img src="cat.jpg">
+        <h1></h1>
+        <p>All the fish: I went to this girl’s party the week after she beat the shit out of my friend. While everyone was getting trashed, 
+            I went around putting tuna inside all the curtain rods and so like weeks went by and they couldn’t figure out why the house smelled like festering death.
+            They caught me through this video where these guys at the party were singing Beyoncé while I was in the background with a can of tuna.?</p>
+        <img src="https://s.isanook.com/me/0/rp/r/w850/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL21lLzAvdWQvMTMvNjYxOTcvYmVlci5qcGc=.jpg">
+        <img src="https://www.khaosod.co.th/wpapp/uploads/2020/04/chok-6.jpg">
         <br>
-        <a href="http://www.it.kmitl.ac.th">This is a link</a>
-
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
-
+        <a href="https://ejudge.it.kmitl.ac.th/auth/login?next=%2Fproblem">E<Judge></a>
+        <a href="https://www.youtube.com">Youtube Link</a>
+        
     </body>
 </html>
